dcd1dc23ba80c4eb28e8efcc98d4826a4c4f8b64
Stéphane Goudeau <stephgou@microsoft.com>
2017-11-16 15:30:43 -0800

    publicIP

--- SQLAlwaysOn/SQLAlwaysOn/DeployAlwaysOn.ps1
+++ SQLAlwaysOn/SQLAlwaysOn/DeployAlwaysOn.ps1
@@ -24,7 +24,7 @@ Write-Host "scriptFolder" $scriptFolder
 set-location $scriptFolder
 
 #endregion init
-Login-AzureRmAccount -SubscriptionId $subscriptionId
+#Login-AzureRmAccount -SubscriptionId $subscriptionId
 
 # Resource group create
 New-AzureRmResourceGroup `
